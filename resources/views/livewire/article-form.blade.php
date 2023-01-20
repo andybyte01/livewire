c6c380e4488138d41ee4ba629d402ac38f8f9b65
Andres Hernandez <andres.122196@gmail.com>
2023-01-20 16:06:47 -0600

    Estilizando el selector de imágenes con Tailwind 0.2

--- resources/views/livewire/article-form.blade.php
+++ resources/views/livewire/article-form.blade.php
@@ -17,8 +17,16 @@
                 <x-slot name="form">
                     <div class="col-span-6 sm:col-span-4 relative">
                         @if ($image)
-                        <x-jet-danger-button class="absolute bottom-2 right-2">{{ __('Change Image') }}</x-jet-danger-button>
+                            <x-jet-danger-button wire:click="$set('image')" class="absolute bottom-2 right-2">
+                                {{ __('Change Image') }}
+                            </x-jet-danger-button>
                             <img src="{{ $image->temporaryUrl() }}" class="border-2 rounded" alt="">
+                        @elseif($article->image)
+                            <x-jet-label for="image" :value="__('Change Image')"
+                                class="absolute bottom-2 right-2 cursor-pointer inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition" />
+                            {{--  <img src="{{ Storage::disk('public')->url($article->image) }}" class="border-2 rounded"
+                                alt=""> --}}
+                            <img src="{{ Storage::url($article->image) }}" class="border-2 rounded" alt="">
                         @else
                             <div
                                 class="h-32 bg-gray-50 border-2 border-dashed rounded flex items-center justify-center">
